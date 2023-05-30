24a36554e5906cbc9da5a2996e8df7e8823cdf79
Maximilian Werk <maximilian.werk@gmx.de>
2023-05-30 13:04:53 +0200

    feat: more code

--- embeddings/app/clip/model.py
+++ embeddings/app/clip/model.py
@@ -4,6 +4,6 @@ model = finetuner.get_model('/mnt/models/clip-osc-run.zip', select_model='clip-t
 
 
 def get_text_sentence_embedding(text: str, normalize: bool = True):
-    bla = finetuner.encode(model=model, data=text)[0]
+    bla = finetuner.encode(model=model, data=[text])[0]
     print(bla)
     return bla
--- embeddings/app/minilm/model.py
+++ embeddings/app/minilm/model.py
@@ -4,4 +4,4 @@ model = finetuner.get_model('/mnt/models/margin-mse-osc-run.zip', device='cpu')
 
 
 def get_text_sentence_embedding(text: str, normalize: bool = True):
-    return finetuner.encode(model=model, data=text)[0]
+    return finetuner.encode(model=model, data=[text])[0]
