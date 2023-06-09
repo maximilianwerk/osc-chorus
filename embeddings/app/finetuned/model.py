0eee364137ceb0c25a9cc8c5d4c87119229f83c3
Maximilian Werk <maximilian.werk@gmx.de>
2023-06-09 15:15:05 +0200

    feat: cleanup embedding serving

---
+++ embeddings/app/finetuned/model.py
@@ -0,0 +1,14 @@
+import finetuner
+
+sbert_model = finetuner.get_model('/mnt/models/fine-tuned-sbert-model.zip', device='cpu')
+
+
+def get_sbert_embedding(text: str, normalize: bool = True):
+    return finetuner.encode(model=sbert_model, data=[text])[0]
+
+
+clip_text_model = finetuner.get_model('/mnt/models/fine-tuned-clip-model.zip', select_model='clip-text', device='cpu')
+
+
+def get_clip_text_embedding(text: str, normalize: bool = True):
+    return finetuner.encode(model=clip_text_model, data=[text])[0]
--- embeddings/app/main.py
+++ embeddings/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from clip import router as router_clip
 from minilm import router as router_minilm
+from finetuned import sbert_router, clip_router
 
 app = FastAPI()
 
 app.include_router(router_clip.router, prefix="/clip")
 app.include_router(router_minilm.router, prefix="/minilm")
+app.include_router(clip_router, prefix="/finetunedclip")
+app.include_router(sbert_router, prefix="/finetunedsbert")
 
 
