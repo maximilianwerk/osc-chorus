e5623592a1c5b2d52c5e7e934d8de9700d385584
Maximilian Werk <maximilian.werk@gmx.de>
2023-06-09 16:28:56 +0200

    feat: finished finetuned vector creation

--- data-encoder/ecommerce/vectors/finetuned_products.py
+++ data-encoder/ecommerce/vectors/finetuned_products.py
@@ -1,39 +1,27 @@
 #!/usr/bin/env python3
 
 import json
-import io
 import os
 import requests
 from zipfile import ZipFile
 
 import finetuner
 
-from PIL import Image
-
-
-PATH_PRODUCTS_DATASET = "data"
-# Name of the zip file (without .zip extension)
-NAME_DATASET = "5.json"
-# Load the CLIP model
-
-PATH_PRODUCTS_VECTORS_JSON = "data/products-vectors-5.json"
 PATH_IMAGES = "data/images"
 
 
-def load_products_dataset():
-    with ZipFile(PATH_PRODUCTS_DATASET+"/"+NAME_DATASET+".zip") as dataZip:
-        with dataZip.open(NAME_DATASET,mode='r') as dataFile:
-            products_dataset = json.load(dataFile)
+def load_products_dataset(input_file):
+    with open(input_file) as dataFile:
+        products_dataset = json.load(dataFile)
     return products_dataset
 
 
-def get_product_sentence(model, product):
-    #print(f"{(product['title'])} {(product['supplier'])}")
+def get_product_sentence(product):
     return f"{(product['title'])} {(product['supplier'])}"
 
 
-def get_products_sentences(model, products_dataset):
-    return [get_product_sentence(model, product) for product in products_dataset]
+def get_products_sentences(products_dataset):
+    return [get_product_sentence(product) for product in products_dataset]
 
 
 def get_product_image(product):
@@ -41,17 +29,8 @@ def get_product_image(product):
     return product_img
 
 
-def get_product_images(products_dataset):
-    return [get_product_image(product) for product in products_dataset]
-
-
-def calculate_product_vector(model, product):
-    product_sentence = get_product_sentence(product)
-    return model.encode(product_sentence)
-
-
 def calculate_products_vectors(model, products_dataset):
-    products_sentences = get_products_sentences(model, products_dataset)
+    products_sentences = get_products_sentences(products_dataset)
 
     return finetuner.encode(model=model, data=products_sentences)
 
@@ -89,34 +68,9 @@ def calculate_products_image_vectors_clip(model, products_dataset):
     return result
 
 
-def export_products_json(products_dataset):
+def export_products_json(products_dataset, output_file):
     # Serializing json
     json_object = json.dumps(products_dataset, indent=2)
     # Writing to dataset.json
-    with open(PATH_PRODUCTS_DATASET+"/"+"products-vectors-"+NAME_DATASET, "w") as outfile:
+    with open(output_file, "w") as outfile:
         outfile.write(json_object)
-
-
-def truncate_sentence(sentence, tokenizer):
-    """
-    Truncate a sentence to fit the CLIP max token limit (77 tokens including the
-    starting and ending tokens).
-
-    Args:
-        sentence(string): The sentence to truncate.
-        tokenizer(CLIPTokenizer): Retrained CLIP tokenizer.
-    """
-
-    cur_sentence = sentence
-    #print("new doc",cur_sentence)
-    tokens = tokenizer.encode(cur_sentence)
-
-    if len(tokens) > 77:
-        # Skip the starting token, only include 75 tokens
-        truncated_tokens = tokens[1:76]
-        cur_sentence = tokenizer.decode(truncated_tokens)
-        # Recursive call here, because the encode(decode()) can have different result
-        return truncate_sentence(cur_sentence, tokenizer)
-
-    else:
-        return cur_sentence
--- embeddings/app/main.py
+++ embeddings/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from clip import router as router_clip
+from clipL14 import router as router_clip
 from minilm import router as router_minilm
-from finetuned import sbert_router, clip_router
+from finetuned.router import sbert_router, clip_router
 
 app = FastAPI()
 
@@ -9,5 +9,3 @@ app.include_router(router_clip.router, prefix="/clip")
 app.include_router(router_minilm.router, prefix="/minilm")
 app.include_router(clip_router, prefix="/finetunedclip")
 app.include_router(sbert_router, prefix="/finetunedsbert")
-
-
